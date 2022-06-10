745164d27d2f60d28b4f6daecce5d4a02117f757
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-10 21:56:22 +0200

    Implements the modifications needed to pass all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(iFrom, iTo) {
+    let tmp = 0; // used to swap parameters if needed
+    let ret = 0;
 
+    if(typeof(iFrom) != 'number' || typeof(iTo) != 'number') {
+        return 'ERROR';
+    }
+
+    if(iFrom > iTo) {
+        tmp = iFrom;
+        iFrom = iTo;
+        iTo = tmp;
+    }
+
+    if(iFrom < 0) {
+        return 'ERROR';
+    }
+
+    for(let i=iFrom; i<=iTo; i++) {
+        ret += i;
+    }
+
+    return ret;
 };
 
 // Do not edit below this line
