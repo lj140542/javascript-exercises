59d5f8d80781248ee51fa51620786392fd9e4edc
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-10 22:09:43 +0200

    Implements the modifications needed to pass all tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(iYear) {
+    if(iYear%100 == 0) {
+        return (iYear%400==0);
+    } else {
+        return (iYear%4==0);
+    }
 };
 
 // Do not edit below this line
