50c4458e6894edbeec8ae2cd89046fed23ce4b94
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-10 21:47:07 +0200

    Implements the modifications needed to pass all the tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(collection) {
+    for(let i = 1; i<arguments.length; i++) {
+        let intruder = arguments[i];
+        while(collection.indexOf(intruder) >= 0) {
+            collection.splice(collection.indexOf(intruder),1);
+        }
+    }
+    
+    return collection;
 };
 
 // Do not edit below this line
