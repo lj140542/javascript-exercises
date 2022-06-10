3614fde439be8a1e359e2cb16f923c9294206635
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-10 21:38:48 +0200

    Implements the modifications needed to pass all the tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(value) {
+    let ret = '';
 
+    for(let i = value.length-1; i>=0; i--) {
+        ret += value[i];
+    }
+
+    return ret;
 };
 
 // Do not edit below this line
