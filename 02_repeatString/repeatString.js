b60bc63a699739b2ca37381a76eaf590ac3e77e9
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-10 21:33:23 +0200

    Implements the modifications needed to pass all the tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(value, nbOfRepeat) {
+    var ret = '';
+    
+    // CHECK FOR NEGATIVE NUMBERS
+    if(nbOfRepeat<0) {
+        return 'ERROR';
+    }
 
+    for(var i = 0; i<nbOfRepeat; i++) {
+        ret += value;
+    }
+
+    return ret;
 };
 
 // Do not edit below this line
