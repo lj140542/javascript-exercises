0c65355b9cb7137a39fbcfd37565b286717fbf2b
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-25 00:27:21 +0200

    Implements the modifications to pass all tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,57 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+
+  for (let cpt = 0; cpt < arr.length; cpt++) {
+    result += arr[cpt];
+  }
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
 
+  let result = arr[0];
+
+  for (let cpt = 1; cpt < arr.length; cpt++) {
+    result = result * arr[cpt];
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  if (y == 0) {
+    return 0;
+  }
+
+  let result = x;
+
+  for (let cpt = 1; cpt < y; cpt++) {
+      result = result * x;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let result = 1;
+
+  for (let cpt = 1; cpt <= x; cpt++) {
+    result = result * cpt;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
