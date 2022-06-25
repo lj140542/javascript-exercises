098202d8cb7b9b06a5ad07182dc466cbe258fb9d
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-25 15:55:42 +0200

    Implements the modifications to pass all tests.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let today = new Date();
+    let calculatedAge = 0;
+    let highestAge = 0;
+    let person;
+    people.forEach(element => {
+        if(element.hasOwnProperty("yearOfDeath")) {
+            calculatedAge = element["yearOfDeath"] - element["yearOfBirth"];
+        } else {
+            calculatedAge = today.getFullYear() - element["yearOfBirth"];
+        }  
+        
+        if(calculatedAge > highestAge) {
+            highestAge = calculatedAge;
+            person = element;
+        } 
+    });
 
+    return person;
 };
 
 // Do not edit below this line
