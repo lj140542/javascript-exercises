433c77366f99e67c949a296a5b2fbe273fe72a75
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-25 15:18:15 +0200

    Implements the modifications to pass all tests.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let bookNames = [];
+    books.forEach(element => {
+        bookNames.push(element["title"]);
+    });
 
+    return bookNames;
 };
 
 // Do not edit below this line
