f7b8f44725453d76274ef4282df1415cdba0e6f4
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-25 15:00:29 +0200

    Implements the modifications to pass all tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (testedString) {
+    testedString = testedString.toUpperCase();
+    testedString = testedString.replaceAll(' ','');
+    testedString = testedString.replace(/[^\w\s]/gi, '')
 
+    let revertedString = '';
+    for (let i = testedString.length-1; i >= 0; i--) {
+        revertedString += testedString[i];
+    }
+
+    return (testedString == revertedString);
 };
 
 // Do not edit below this line
