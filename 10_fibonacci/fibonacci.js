8484dea7171400174dbf0c7bac61e4a9bb004942
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-25 15:11:43 +0200

    Implements the modifications to pass all tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    let tmp = 0;
+    let first = 0;
+    let second = 1;
 
+    if (x <= 0) {
+        return "OOPS";
+    }
+
+    for (let cpt = 1; cpt < x; cpt++) {
+        tmp = second;
+        second += first;
+        first = tmp;
+    }
+
+    return second;
 };
 
 // Do not edit below this line
